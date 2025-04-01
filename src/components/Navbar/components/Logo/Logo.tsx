cf91072ba5e4701b42070b67b3698e7a1c35a193
aZong <giongmua2004@gmail.com>
2025-04-02 00:04:41 +0700

    feat: 🎸 use page context

--- src/components/Navbar/components/Logo/Logo.tsx
+++ src/components/Navbar/components/Logo/Logo.tsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
-import { PagePath } from '../../../../enums';
+import { PagePath, Pages } from '../../../../enums';
+import usePageContext from '../../../../hooks';
 
 function Logo() {
+  const { setCurrentPage } = usePageContext();
+
   return (
-    <Link to={PagePath.Home}>
+    <Link to={PagePath.Home} onClick={() => setCurrentPage(Pages.Home)}>
       <div className="flex items-center gap-3">
         <img src="/images/azong-book.png" className="h-[30px] w-[30px]" />
         <h1 className="font-poppins text-sm font-bold text-white sm:text-lg md:text-xl lg:text-2xl">
