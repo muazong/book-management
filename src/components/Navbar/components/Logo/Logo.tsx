536c8436093b9edbab27c3dde640bc4cc06f2ec7
aZong <giongmua2004@gmail.com>
2025-04-01 22:09:36 +0700

    refactor: 💡 move logo component into folder & add Link to home

---
+++ src/components/Navbar/components/Logo/Logo.tsx
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom';
+import { PagePath } from '../../../../enums';
+
+function Logo() {
+  return (
+    <Link to={PagePath.Home}>
+      <div className="flex items-center gap-3">
+        <img src="/images/azong-book.png" className="h-[30px] w-[30px]" />
+        <h1 className="font-poppins text-sm font-bold text-white sm:text-lg md:text-xl lg:text-2xl">
+          aZongBook
+        </h1>
+      </div>
+    </Link>
+  );
+}
+
+export default Logo;
