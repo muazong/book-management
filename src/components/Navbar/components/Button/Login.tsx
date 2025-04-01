08b830172289296b9bc561625db78c56eb68b070
aZong <giongmua2004@gmail.com>
2025-04-01 16:12:59 +0700

    feat: 🎸 add button

---
+++ src/components/Navbar/components/Button/Login.tsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <div className="font-bold text-white">
+      <button className="hover:bg-black-400 cursor-pointer rounded-sm border-2 border-solid border-blue-500 bg-blue-500 px-5 py-1 transition-colors duration-200 ease-in-out hover:border-blue-200">
+        Login
+      </button>
+    </div>
+  );
+}
+
+export default Login;
