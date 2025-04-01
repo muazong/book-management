1e6ac4739f32145fc39e66b0986a07e02e0cb69d
aZong <giongmua2004@gmail.com>
2025-04-01 15:10:16 +0700

    feat: 🎸 add navigation component for navbar

---
+++ src/components/Navbar/components/Navigations.tsx
@@ -0,0 +1,13 @@
+function Navigations() {
+  return (
+    <div className="text-white">
+      <ul className="font-poppins flex items-center justify-between gap-6 font-semibold">
+        <li className="cursor-pointer hover:underline">Home</li>
+        <li className="cursor-pointer hover:underline">All Books</li>
+        <li className="cursor-pointer hover:underline">About Us</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigations;
