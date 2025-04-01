f5acb90dbe2fb80fff9dc6f03d59dcfb7ac61118
aZong <giongmua2004@gmail.com>
2025-04-01 15:10:06 +0700

    feat: 🎸 add logo component for navbar

---
+++ src/components/Navbar/components/Logo.tsx
@@ -0,0 +1,10 @@
+function Logo() {
+  return (
+    <div className="flex items-center gap-3">
+      <img src="/images/azong-book.png" className="h-[50px] w-[50px]" />
+      <h1 className="font-poppins text-2xl font-bold text-white">aZongBook</h1>
+    </div>
+  );
+}
+
+export default Logo;
