4dbc21975fcd30c169d9a8d443e748d93013319b
aZong <giongmua2004@gmail.com>
2025-04-01 15:07:16 +0700

    feat: 🎸 add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Navbar from './components/Navbar';
+
 function App() {
-  return <div className="bg-black-500 h-screen"></div>;
+  return (
+    <div className="bg-black-500 container">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
