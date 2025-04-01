5e00a49206fba7550e99c7c77e0493ea98bbc62d
aZong <giongmua2004@gmail.com>
2025-04-02 00:03:49 +0700

    feat: 🎸 add usePageContext hook

---
+++ src/hooks/usePageContext.ts
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import { PageContext } from '../context';
+
+function usePageContext() {
+  const pageContext = useContext(PageContext);
+
+  if (pageContext === null) {
+    throw new Error('usePageContext must be used with a PageContext');
+  }
+
+  return { ...pageContext };
+}
+
+export default usePageContext;
