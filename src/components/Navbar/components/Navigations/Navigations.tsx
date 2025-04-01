9e8298237a4789da9a35606561bc29a193fb8fea
aZong <giongmua2004@gmail.com>
2025-04-01 18:45:13 +0700

    feat: 🎸 responsive & add menu modal for mobile screen

--- src/components/Navbar/components/Navigations/Navigations.tsx
+++ src/components/Navbar/components/Navigations/Navigations.tsx
@@ -3,6 +3,7 @@ import { Pages } from '../../../../enums';
 
 function Navigations() {
   const [currentPage, setCurrentPage] = useState<Pages>(Pages.Home);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
   const navRef = useRef<HTMLUListElement>(null);
   const underlineRef = useRef<HTMLDivElement>(null);
 
@@ -20,27 +21,58 @@ function Navigations() {
   }, [currentPage]);
 
   return (
-    <div className="relative text-white">
-      <ul
-        ref={navRef}
-        className="font-poppins relative flex items-center justify-between gap-6 font-semibold"
-      >
-        {Object.values(Pages).map((value) => (
-          <li
-            key={value}
-            onClick={() => setCurrentPage(value)}
-            className="relative cursor-pointer px-2 py-1"
-          >
-            <p>{value}</p>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <div className="relative hidden sm:block">
+        <ul
+          ref={navRef}
+          className="font-poppins relative flex items-center justify-between gap-6 font-semibold text-white"
+        >
+          {Object.values(Pages).map((value) => (
+            <li
+              key={value}
+              onClick={() => setCurrentPage(value)}
+              className="relative cursor-pointer px-2 py-1"
+            >
+              <p>{value}</p>
+            </li>
+          ))}
+        </ul>
 
-      {/* Underline */}
-      <div
-        ref={underlineRef}
-        className="absolute bottom-0 h-[2px] bg-white transition-all duration-300 ease-in-out"
-      ></div>
+        {/* Underline */}
+        <div
+          ref={underlineRef}
+          className="absolute bottom-0 h-[2px] bg-white transition-all duration-300 ease-in-out"
+        ></div>
+      </div>
+
+      {/* Mobile  */}
+      <div className="block sm:hidden">
+        <button
+          className="cursor-pointer text-2xl text-white"
+          onClick={() => setIsMenuOpen(true)}
+        >
+          &#9776;
+        </button>
+
+        {isMenuOpen && (
+          <div className="bg-black-500 absolute top-0 right-0 left-0 h-screen w-full">
+            <button
+              className="absolute right-0 cursor-pointer p-3 text-3xl text-white"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              &times;
+            </button>
+            <ul className="font-poppins flex h-full flex-col items-center justify-center gap-3 text-2xl text-white">
+              <li className="relative cursor-pointer px-2 py-1">Login</li>
+              {Object.values(Pages).map((value) => (
+                <li key={value} className="relative cursor-pointer px-2 py-1">
+                  <p>{value}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
