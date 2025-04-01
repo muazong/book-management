53b5330f71d5f961fbea867c8e6cbe3af0be98c2
aZong <giongmua2004@gmail.com>
2025-04-01 21:08:32 +0700

    feat: 🎸 use router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="bg-black-500 container mx-auto">
       <Navbar />
+      <Outlet />
+      {/* <Footer /> */}
     </div>
   );
 }
