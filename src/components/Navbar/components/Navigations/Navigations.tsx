18a3959252f2bf4e83cf6160c9ae9e96818a0869
aZong <giongmua2004@gmail.com>
2025-04-02 00:25:26 +0700

    refactor: 💡 remove all states

--- src/components/Navbar/components/Navigations/Navigations.tsx
+++ src/components/Navbar/components/Navigations/Navigations.tsx
@@ -1,33 +1,11 @@
-import { useState } from 'react';
-import { Pages } from '../../../../enums';
-
 import MobileNav from './responsive/MobileNav';
 import LargeScreenNav from './responsive/LargeScreenNav';
 
 function Navigations() {
-  const [currentPage, setCurrentPage] = useState<Pages>(Pages.Home);
-  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-
-  const setCurrentPageHandler = (page: Pages) => {
-    setCurrentPage(page);
-
-    // This logic should only be executed on mobile screen size
-    if (isMenuOpen) {
-      setIsMenuOpen(false);
-    }
-  };
-
   return (
     <div>
-      <LargeScreenNav
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-      />
-      <MobileNav
-        isMenuOpen={isMenuOpen}
-        setIsMenuOpen={setIsMenuOpen}
-        setCurrentPageHandler={setCurrentPageHandler}
-      />
+      <LargeScreenNav />
+      <MobileNav />
     </div>
   );
 }
