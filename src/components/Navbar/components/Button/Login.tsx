af9410dbf5bca4097e37ff4274fa32c4cb725517
aZong <giongmua2004@gmail.com>
2025-04-08 22:28:29 +0700

    feat: add Link for login button

--- src/components/Navbar/components/Button/Login.tsx
+++ src/components/Navbar/components/Button/Login.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function Login() {
   return (
     <div className="font-bold text-white">
-      <button className="hover:bg-black-400 cursor-pointer rounded-sm border-2 border-solid border-blue-500 bg-blue-500 px-5 py-1 transition-colors duration-200 ease-in-out hover:border-blue-200">
-        Login
-      </button>
+      <Link to="/login">
+        <button className="hover:bg-black-400 cursor-pointer rounded-sm border-2 border-solid border-blue-500 bg-blue-500 px-5 py-1 transition-colors duration-200 ease-in-out hover:border-blue-200">
+          Login
+        </button>
+      </Link>
     </div>
   );
 }
