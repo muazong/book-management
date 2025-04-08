64e07883319bc899f14b7839fe42a6c79255cf7b
aZong <giongmua2004@gmail.com>
2025-04-08 23:43:48 +0700

    feat: useAuthContext hook

--- src/utils/PrivateRoutes.tsx
+++ src/utils/PrivateRoutes.tsx
@@ -1,9 +1,11 @@
+import { PagePath } from '@/enums';
+import { useAuthContext } from '@/hooks';
 import { Navigate, Outlet } from 'react-router-dom';
 
 function PrivateRoutes() {
-  const user = true;
+  const { user } = useAuthContext();
 
-  return <>{user ? <Outlet /> : <Navigate to={'/login'} />}</>;
+  return <>{user ? <Outlet /> : <Navigate to={PagePath.Login} />}</>;
 }
 
 export default PrivateRoutes;
