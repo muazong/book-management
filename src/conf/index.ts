28959a681344a0a836a102fab774efd4029c67ee
aZong <giongmua2004@gmail.com>
2025-04-08 20:16:22 +0700

    feat: config file

---
+++ src/conf/index.ts
@@ -0,0 +1,9 @@
+const conf = {
+  base_url: import.meta.env.VITE_APPWRITE_URL,
+  project_id: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  database_id: import.meta.env.VITE_APPWRITE_DATABASE_ID,
+  collection_id: import.meta.env.VITE_APPWRITE_COLLECTION_ID,
+  bucket_id: import.meta.env.VITE_APPWRITE_BUCKET_ID,
+};
+
+export default conf;
