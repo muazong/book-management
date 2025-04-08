da6be1857d0bad2d91240733b8a242a3339805fd
aZong <giongmua2004@gmail.com>
2025-04-08 23:43:09 +0700

    feat: check user login

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,20 @@
-import { Link } from 'react-router-dom';
+import { PagePath } from '@/enums';
+import { useAuthContext } from '@/hooks';
+import { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function LoginPage() {
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) navigate(PagePath.Home);
+  }, [user, navigate]);
+
   return (
     <div>
       <p className="text-white">Login Page</p>
-      <Link to="/">Home</Link>
+      <Link to={PagePath.Home}>Home</Link>
     </div>
   );
 }
