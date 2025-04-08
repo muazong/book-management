168d9997f9918ea019cfb70bc61a66d9c81821c9
aZong <giongmua2004@gmail.com>
2025-04-08 22:31:53 +0700

    feat: PrivateRoutes

---
+++ src/utils/PrivateRoutes.tsx
@@ -0,0 +1,9 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+function PrivateRoutes() {
+  const user = true;
+
+  return <>{user ? <Outlet /> : <Navigate to={'/login'} />}</>;
+}
+
+export default PrivateRoutes;
