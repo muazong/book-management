02c0ca4ed50d124161e44792539efce1b2107bf8
aZong <giongmua2004@gmail.com>
2025-04-08 14:29:06 +0700

    fix: sync url

--- src/components/Navbar/components/Navigations/responsive/LargeScreenNav.tsx
+++ src/components/Navbar/components/Navigations/responsive/LargeScreenNav.tsx
@@ -1,18 +1,32 @@
 import { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Pages, PagePath } from '@/enums';
 import usePageContext from '@/hooks';
 
-const breakpoint = 640; // breakpoint for small screen
+const breakpoint = 640;
 
 function LargeScreenNav() {
   const { currentPage, setCurrentPage } = usePageContext();
-  const [isDesktop, setIsDesktop] = useState<boolean>(
-    window.innerWidth >= breakpoint,
-  );
-
+  const location = useLocation();
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth >= breakpoint);
   const navRef = useRef<HTMLUListElement>(null);
   const underlineRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case PagePath.Home:
+        setCurrentPage(Pages.Home);
+        break;
+      case PagePath.AllBooks:
+        setCurrentPage(Pages.AllBooks);
+        break;
+      case PagePath.AboutUs:
+        setCurrentPage(Pages.AboutUs);
+        break;
+      default:
+        break;
+    }
+  }, [location.pathname, setCurrentPage]);
 
   useEffect(() => {
     const handleResize = () => {
@@ -40,33 +54,23 @@ function LargeScreenNav() {
         ref={navRef}
         className="font-poppins relative flex items-center justify-between gap-6 font-semibold text-white"
       >
-        <li
-          className="relative cursor-pointer px-2 py-1"
-          onClick={() => setCurrentPage(Pages.Home)}
-        >
+        <li className="relative cursor-pointer px-2 py-1">
           <Link to={PagePath.Home}>
             <p>Home</p>
           </Link>
         </li>
-        <li
-          className="relative cursor-pointer px-2 py-1"
-          onClick={() => setCurrentPage(Pages.AllBooks)}
-        >
+        <li className="relative cursor-pointer px-2 py-1">
           <Link to={PagePath.AllBooks}>
             <p>All Books</p>
           </Link>
         </li>
-        <li
-          className="relative cursor-pointer px-2 py-1"
-          onClick={() => setCurrentPage(Pages.AboutUs)}
-        >
+        <li className="relative cursor-pointer px-2 py-1">
           <Link to={PagePath.AboutUs}>
             <p>About Us</p>
           </Link>
         </li>
       </ul>
 
-      {/* Underline */}
       <div
         ref={underlineRef}
         className="absolute bottom-0 h-[2px] bg-white transition-all duration-300 ease-in-out"
