83515a92363d65d1cdbeeba294b6f7f34fe73917
aZong <giongmua2004@gmail.com>
2025-04-08 22:30:01 +0700

    feat: Main

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,11 @@
+import PrivateRoutes from '@/utils/PrivateRoutes';
+
+function Main() {
+  return (
+    <main className="my-10">
+      <PrivateRoutes />
+    </main>
+  );
+}
+
+export default Main;
