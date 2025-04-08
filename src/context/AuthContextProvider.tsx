74a581f7d4dc260fc07a52db5b3d26611672d988
aZong <giongmua2004@gmail.com>
2025-04-09 01:10:51 +0700

    feat: logout, login, check current user

--- src/context/AuthContextProvider.tsx
+++ src/context/AuthContextProvider.tsx
@@ -1,9 +1,13 @@
-import { createContext, ReactNode, useState } from 'react';
+import appwriteAuth from '@/appwrite/auth';
+import { UserInfo } from '@/interfaces';
+import { Models } from 'appwrite';
+import { createContext, ReactNode, useEffect, useState } from 'react';
 
 interface AuthContextProps {
-  user: boolean | null;
-  setUser: React.Dispatch<React.SetStateAction<boolean | null>>;
+  user: Models.Session | Models.User<Models.Preferences> | null;
+  setUser: (user: Models.Session | null) => void;
   authLogout: () => void;
+  authLogin: (userInfo: UserInfo) => void;
 }
 
 interface AuthContextProviderProps {
@@ -13,14 +17,45 @@ interface AuthContextProviderProps {
 const AuthContext = createContext<AuthContextProps | null>(null);
 
 function AuthContextProvider({ children }: AuthContextProviderProps) {
-  const [user, setUser] = useState<boolean | null>(true);
+  const [user, setUser] = useState<
+    Models.Session | Models.User<Models.Preferences> | null
+  >(null);
 
-  const authLogout = () => {
-    setUser(false);
+  useEffect(() => {
+    checkUserStatus();
+  }, []);
+
+  const authLogout = async () => {
+    try {
+      await appwriteAuth.deleteSessions();
+    } catch (error) {
+      console.error('authLogout() :: ', error);
+    } finally {
+      setUser(null);
+    }
+  };
+  const authLogin = async (userInfo: UserInfo) => {
+    try {
+      await appwriteAuth.deleteSessions();
+      const response = await appwriteAuth.createUser(userInfo);
+
+      if (response) setUser(response);
+    } catch (error) {
+      console.error('authLogin() :: ', error);
+    }
+  };
+  const checkUserStatus = async () => {
+    try {
+      const currentUser = await appwriteAuth.getCurrentUser();
+
+      if (currentUser) setUser(currentUser);
+    } catch (error) {
+      console.error('checkUserStatus() :: ', error);
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ user, setUser, authLogout }}>
+    <AuthContext.Provider value={{ user, setUser, authLogout, authLogin }}>
       {children}
     </AuthContext.Provider>
   );
