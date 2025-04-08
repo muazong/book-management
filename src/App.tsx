a99268f00a513e88a637414bc204d114feaf71f5
aZong <giongmua2004@gmail.com>
2025-04-08 23:07:58 +0700

    feat: use AuthContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import Footer from './components/Footer';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
-import { PageContextProvider } from './context/PageContextProvider';
+import { AuthContextProvider, PageContextProvider } from './context';
 
 function App() {
   return (
-    <PageContextProvider>
-      <div className="bg-black-500 container mx-auto px-7">
-        <Navbar />
-        <Main />
-        <Footer />
-      </div>
-    </PageContextProvider>
+    <AuthContextProvider>
+      <PageContextProvider>
+        <div className="bg-black-500 container mx-auto px-7">
+          <Navbar />
+          <Main />
+          <Footer />
+        </div>
+      </PageContextProvider>
+    </AuthContextProvider>
   );
 }
 
