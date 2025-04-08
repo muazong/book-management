47e59c5d75dee975ebb7fed1d633de03425f36aa
aZong <giongmua2004@gmail.com>
2025-04-08 23:29:48 +0700

    fix: auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import Footer from './components/Footer';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
-import { AuthContextProvider, PageContextProvider } from './context';
+import { PageContextProvider } from './context';
 
 function App() {
   return (
-    <AuthContextProvider>
-      <PageContextProvider>
-        <div className="bg-black-500 container mx-auto px-7">
-          <Navbar />
-          <Main />
-          <Footer />
-        </div>
-      </PageContextProvider>
-    </AuthContextProvider>
+    <PageContextProvider>
+      <div className="bg-black-500 container mx-auto px-7">
+        <Navbar />
+        <Main />
+        <Footer />
+      </div>
+    </PageContextProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 
 import AppRouter from './routes';
+import { AuthContextProvider } from './context';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <AppRouter />
+    <AuthContextProvider>
+      <AppRouter />
+    </AuthContextProvider>
   </StrictMode>,
 );
