1681322930b56c32653b2b503d93fc02cd3386ed
aZong <giongmua2004@gmail.com>
2025-04-08 22:40:58 +0700

    feat: LoadingPage

---
+++ src/pages/LoadingPage/LoadingPage.tsx
@@ -0,0 +1,9 @@
+function LoadingPage() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <p className="font-montserrat text-3xl text-white">Loading...</p>
+    </div>
+  );
+}
+
+export default LoadingPage;
