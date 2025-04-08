47d1210203c114e5391ebf8f856ee56b0ca9ce88
aZong <giongmua2004@gmail.com>
2025-04-08 23:43:23 +0700

    feat: check user login

--- src/components/Navbar/components/Button/Login.tsx
+++ src/components/Navbar/components/Button/Login.tsx
@@ -1,13 +1,24 @@
+import { PagePath } from '@/enums';
+import { useAuthContext } from '@/hooks';
 import { Link } from 'react-router-dom';
 
+const styles =
+  'hover:bg-black-400 cursor-pointer rounded-sm border-2 border-solid border-blue-500 bg-blue-500 px-5 py-1 transition-colors duration-200 ease-in-out hover:border-blue-200';
+
 function Login() {
+  const { user, authLogout } = useAuthContext();
+
   return (
     <div className="font-bold text-white">
-      <Link to="/login">
-        <button className="hover:bg-black-400 cursor-pointer rounded-sm border-2 border-solid border-blue-500 bg-blue-500 px-5 py-1 transition-colors duration-200 ease-in-out hover:border-blue-200">
-          Login
+      {user ? (
+        <button className={styles} onClick={authLogout}>
+          Logout
         </button>
-      </Link>
+      ) : (
+        <Link to={PagePath.Login}>
+          <button className={styles}>Login</button>
+        </Link>
+      )}
     </div>
   );
 }
