1be77f835eb6ede94c53dab6b57712ba93199be4
aZong <giongmua2004@gmail.com>
2025-04-08 22:59:11 +0700

    feat: add loading

--- src/context/PageContextProvider.tsx
+++ src/context/PageContextProvider.tsx
@@ -1,7 +1,10 @@
-import { createContext, ReactNode, useState } from 'react';
-import { Pages } from '../enums';
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import { Pages } from '@/enums';
+import LoadingPage from '@/pages/LoadingPage';
 
 interface PageContextProps {
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
   currentPage: Pages;
   setCurrentPage: (page: Pages) => void;
 }
@@ -14,10 +17,23 @@ const PageContext = createContext<PageContextProps | null>(null);
 
 function PageContextProvider({ children }: PageContextProviderProps) {
   const [currentPage, setCurrentPage] = useState<Pages>(Pages.Home);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => setLoading(false), 3000);
+    return () => clearTimeout(timeoutId);
+  }, []);
 
   return (
-    <PageContext.Provider value={{ currentPage, setCurrentPage }}>
-      {children}
+    <PageContext.Provider
+      value={{
+        loading,
+        setLoading,
+        currentPage,
+        setCurrentPage,
+      }}
+    >
+      {loading ? <LoadingPage /> : children}
     </PageContext.Provider>
   );
 }
