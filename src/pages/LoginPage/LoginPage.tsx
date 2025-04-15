9f57c4b49148debcea5636aa53c6030312f71776
aZong <giongmua2004@gmail.com>
2025-04-16 00:32:31 +0700

    refactor: style

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,13 +2,8 @@ import LoginForm from './LoginForm';
 
 function LoginPage() {
   return (
-    <div className="container flex h-screen items-center justify-center">
-      <div className="h-[560px] w-[500px] rounded-md border-2 border-[rgba(255,255,255,0.4)] bg-[rgba(255,255,255,0.4)] p-4">
-        <h1 className="font-montserrat text-center text-3xl font-bold text-white">
-          Login
-        </h1>
-        <LoginForm />
-      </div>
+    <div className="container flex h-screen flex-col items-center justify-center">
+      <LoginForm />
     </div>
   );
 }
