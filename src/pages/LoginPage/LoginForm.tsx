9382fd186dfed233183a36e3045876c4ba1c39b3
aZong <giongmua2004@gmail.com>
2025-04-16 00:32:45 +0700

    feat: register feture

--- src/pages/LoginPage/LoginForm.tsx
+++ src/pages/LoginPage/LoginForm.tsx
@@ -1,62 +1,167 @@
 import { PagePath } from '@/enums';
 import { useAuthContext } from '@/hooks';
-import { FormEvent, useEffect, useRef } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function LoginForm() {
-  const { user, login } = useAuthContext();
   const navigate = useNavigate();
+  const {
+    user,
+    login,
+    register,
+    isRegistering,
+    setIsRegistering,
+    errorMessage,
+    setErrorMessage,
+  } = useAuthContext();
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
 
   useEffect(() => {
     if (user) navigate(PagePath.Home);
   }, [user, navigate]);
 
-  const formRef = useRef<HTMLFormElement | null>(null);
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
 
-  const handleSubmit = (el: FormEvent<HTMLFormElement>) => {
-    if (!formRef.current) return;
-
-    el.preventDefault();
-    const formData = new FormData(formRef.current);
-    const email = formData.get('email')?.toString().trim() ?? '';
-    const password = formData.get('password')?.toString().trim() ?? '';
-
-    login(email, password);
+    if (isRegistering) {
+      if (confirmPassword !== password) {
+        setErrorMessage('Password do not match!');
+      } else {
+        setErrorMessage('');
+        register(email, password, name);
+        login(email, password);
+      }
+    } else {
+      login(email, password);
+      setErrorMessage('');
+    }
   };
 
   return (
     <>
-      <form ref={formRef} onSubmit={handleSubmit}>
-        <div className="my-5">
-          <label className="text-lg text-white" htmlFor="email">
+      <form
+        onSubmit={handleSubmit}
+        className="relative h-[500px] w-[500px] rounded-sm bg-white p-5"
+      >
+        <h1 className="font-montserrat text-center text-3xl font-bold">
+          {!isRegistering ? 'Login' : 'Sign Up'}
+        </h1>
+
+        {/* Name */}
+        {isRegistering && (
+          <div className="mb-7">
+            <label className="text-lg" htmlFor="name">
+              Name:
+            </label>
+            <input
+              className="font-roboto border-black-400 h-[40px] w-full border p-3 text-lg outline-none placeholder:text-sm"
+              type="text"
+              name="name"
+              id="name"
+              autoComplete="off"
+              required
+              value={name}
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setName(event.target.value.trim())
+              }
+              placeholder="John Wick"
+            />
+          </div>
+        )}
+
+        {/* Email */}
+        <div>
+          <label className="text-lg" htmlFor="email">
             Email:
           </label>
           <input
-            className="font-roboto h-[40px] w-full border border-white p-3 text-lg text-white outline-none"
+            className="font-roboto border-black-400 h-[40px] w-full border p-3 text-lg outline-none placeholder:text-sm"
             type="email"
             name="email"
             id="email"
+            autoComplete="off"
+            required
+            value={email}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              setEmail(event.target.value.trim())
+            }
+            placeholder="sample@gmail.com"
           />
         </div>
 
-        <div className="my-5">
-          <label className="text-lg text-white" htmlFor="password">
+        {/* Password */}
+        <div className="my-7">
+          <label className="text-lg" htmlFor="password">
             Password:
           </label>
           <input
-            className="font-roboto h-[40px] w-full border border-white p-3 text-lg text-white outline-none"
+            className="font-roboto border-black-400 h-[40px] w-full border p-3 text-lg outline-none placeholder:text-sm"
             type="password"
             name="password"
             id="password"
+            required
+            autoComplete="off"
+            value={password}
+            onChange={(event: ChangeEvent<HTMLInputElement>) =>
+              setPassword(event.target.value.trim())
+            }
+            placeholder="Enter your password..."
           />
         </div>
 
-        <button
-          type="submit"
-          className="rounded-sm border-none bg-blue-500 px-4 py-2 text-white hover:bg-blue-400"
-        >
-          Submit
-        </button>
+        {/* Confirm Password */}
+        {isRegistering && (
+          <div className="my-5">
+            <label className="text-lg text-black" htmlFor="confirm-password">
+              Confirm Password:
+            </label>
+            <input
+              className="font-roboto border-black-400 h-[40px] w-full border p-3 text-lg outline-none placeholder:text-sm"
+              type="password"
+              name="confirm-password"
+              id="confirm-password"
+              autoComplete="off"
+              value={confirmPassword}
+              onChange={(event: ChangeEvent<HTMLInputElement>) =>
+                setConfirmPassword(event.target.value.trim())
+              }
+              required
+              placeholder="Please confirm your password..."
+            />
+          </div>
+        )}
+
+        <span className="text-sm text-red-500">{errorMessage}</span>
+
+        <div className="text-right">
+          <button
+            type="submit"
+            className="cursor-pointer rounded-sm border-none bg-blue-500 px-4 py-2 text-black hover:bg-blue-400"
+          >
+            {!isRegistering ? 'Login' : 'Sign Up'}
+          </button>
+        </div>
+
+        <div className="absolute bottom-3">
+          {!isRegistering ? (
+            <p className="text-sm" onClick={() => setIsRegistering(true)}>
+              Don't have an account?
+              <a href="#" className="text-cyan-700">
+                &nbsp;Sign up
+              </a>
+            </p>
+          ) : (
+            <p className="text-sm" onClick={() => setIsRegistering(false)}>
+              Already have an account?
+              <a href="#" className="text-cyan-700">
+                &nbsp;Log in
+              </a>
+            </p>
+          )}
+        </div>
       </form>
     </>
   );
